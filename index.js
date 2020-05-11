bca06d837ca8e3bea6930616e5b61c0a19445dc4
Ashean <ashean80@hotmail.it>
2020-05-11 23:06:06 +0200

    try a new web socket - faye

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+var WebSocket = require('faye-websocket')
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+//const io = require('socket.io')(http);
 //ste the statics
 app.use(bodyParser.json())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
 app.use("/public", express.static("public"));
 
-io.set('transports', ['websocket']);
+//io.set('transports', ['websocket']);
 
 // set view engine
 app.set("view engine", "ejs");
@@ -36,11 +37,25 @@ db.authenticate()
 
 
 //Set Sockets
-io.on('connection',(socket)=>{
+http.on('upgrade', function(request, socket, body) {
+  if (WebSocket.isWebSocket(request)) {
+    var ws = new WebSocket(request, socket, body);
+    
+    ws.on('message', function(event) {
+      ws.send(event.data);
+    });
+    
+    ws.on('close', function(event) {
+      console.log('close', event.code, event.reason);
+      ws = null;
+    });
+  }
+});
+/*io.on('connection',(socket)=>{
     socket.emit('welcome', 'Hello There and Welcome to the Socket');
     console.log("A new client is connected")
 })
-
+*/
 
 
 app.use('/', require ('./route/mobile'));
